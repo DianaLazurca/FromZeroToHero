2e5b24c89e946e85fd741ed31759b388fb5feba3
dianaLazurca <dianna.lazurca@gmail.com>
2015-05-18 13:24:06 +0300

    finished views

--- Classes/HotelList.cs
+++ Classes/HotelList.cs
@@ -6,16 +6,31 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class HotelList
+    public  static class HotelList
     {
-        private List<Hotel> hotels;
 
-        public HotelList()
+        public static  List<Hotel> Hotels 
         {
-            Hotels = new List<Hotel>();
-        }
+            
+            get {
+                List<Hotel> hotels = new List<Hotel>();
 
-        public List<Hotel> Hotels { get; set; }
+                Room room1 = new Room("room1", 1, 12, 2, RoomTypes.Single);
+                Room room2 = new Room("room2", 2, 14, 4, RoomTypes.Double);
+
+                Room[] rooms = new Room[2] { room1, room2 };
+                Hotel hotelClass = new Hotel(1, "Hoooteeel", "desc hotel1", "My address", 3, 30, DateTime.Now, rooms, 5);
+                Hotel hotelClass1 = new Hotel(2, "Hoooteeel1", "desc hotel2", "My address", 1, 30, DateTime.Now, rooms, 5);
+
+                Hotel hotelClass2 = new Hotel(4, "Hoooteeel2", "desc hotel3", "My address", 1, 30, DateTime.Now, rooms, 5);
+
+                hotels.Add(hotelClass);
+                hotels.Add(hotelClass1);
+                hotels.Add(hotelClass2); 
+                return hotels;
+            }
+           
+        }
 
     }
 }
--- FZTH.MVC/Controllers/HotelController.cs
+++ FZTH.MVC/Controllers/HotelController.cs
@@ -9,36 +9,25 @@ namespace FZTH.MVC.Controllers
 {
     public class HotelController : Controller
     {
-        private HotelList hotels = new HotelList(); 
+       
         //
         // GET: /Hotel/
         [HttpGet]
         public ActionResult Index()
         {
-            Room room1 = new Room("room1", 1, 12, 2, RoomTypes.Single);
-            Room room2 = new Room("room2", 2, 14, 4, RoomTypes.Double);
-
-            Room[] rooms = new Room[2] { room1, room2 };
-            Hotel hotelClass = new Hotel(1, "Hoooteeel", "desc hotel", "My address", 3, 30, DateTime.Now, rooms, 5);
-            Hotel hotelClass1 = new Hotel(2, "Hoooteeel1", "desc hotel", "My address", 1, 30, DateTime.Now, rooms, 5);
-
-            hotels.Hotels.Add(hotelClass);
-            hotels.Hotels.Add(hotelClass1);
-
-            ViewBag.MyList = hotels.Hotels;
-            
+        
 
             return View("Index");
         }
         
         public ActionResult Detail(int id)
         {
-            var hotel = hotels.Hotels.FirstOrDefault(x => x.Id == id);
+            /*var hotel = hotels.Hotels.FirstOrDefault(x => x.Id == id);
 
             if (hotel != null)
             {
                 return Json(hotel, JsonRequestBehavior.AllowGet);
-            }
+            }*/
             return HttpNotFound();
 
         }
